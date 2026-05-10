37d7529a7215fde1cc329dec9e45ae92e51c1c94
Aman Singh <amansingh43777@gmail.com>
2026-05-11 01:25:03 +0530

    Remove unused imports

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
+import os
+from datetime import datetime
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from requests import post
-from sympy import content
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
-import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
